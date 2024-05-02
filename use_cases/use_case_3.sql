d6f773cf3693d1031d5f1a054913341489862e27
Svelte <jamesmuking@gmail.com>
2024-05-02 19:20:00 +0800

    complete_use_case_3

---
+++ use_cases/use_case_3.sql
@@ -0,0 +1,28 @@
+-- Use Case 3: based on the email address from property_owner table, get all properties including type description
+-- Sample to use: email = 'citzcovich8@weebly.com'
+-- Sample output: property_id, property_type, property_Desc , user_id, user_name, email
+
+DROP PROCEDURE IF EXISTS GetPropertiesByEmail; -- Drop the procedure after use
+
+DELIMITER //
+CREATE PROCEDURE GetPropertiesByEmail(IN email_address VARCHAR(64))
+BEGIN
+    SELECT
+        p.property_id,
+        pt.pType_id,
+        pt.pType_Desc,
+        po.user_id,
+        po.user_name,
+        po.user_email
+
+    FROM
+        property p
+    JOIN property_type pt ON p.pType_id = pt.pType_id
+    JOIN property_owner po ON p.user_id = po.user_id
+    WHERE
+        po.user_email = email_address;
+END //
+DELIMITER ;
+
+CALL GetPropertiesByEmail('citzcovich8@weebly.com'); -- Call the procedure
+DROP PROCEDURE IF EXISTS GetPropertiesByEmail; -- Drop the procedure after use
